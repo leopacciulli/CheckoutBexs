1161705b877ad473b6a396f3a2b266bfcabc4367
Leonardo Pacciulli <leonardo.pacciulli@opah.com.br>
2020-05-05 21:36:03 -0300

    Instalação jest

---
+++ src/__tests__/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, configure, mount } from 'enzyme';
+import Checkout from '../pages/Checkout';
+
+describe('Checkout', () => {
+  it('Renderizar tela', () => {
+    // const render = renderer.create(<Checkout />);
+    // expect(render).toMatchSnapshot();
+  });
+});
